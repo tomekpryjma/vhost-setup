0ba7c8aad5995f6f91d638ba828168748060a5b4
Tomek <tompe97@tlen.pl>
2019-10-03 23:56:18 +0100

    Attempt system support checks

--- vhost/VHostManager.py
+++ vhost/VHostManager.py
@@ -1,14 +1,22 @@
 from vhost.VHostCreate import VHostCreate
+from vhost.system_support import os_is_supported, server_is_supported
 
 class VHostManager:
     def __init__(self, config):
         self.config = config
     
     def init(self):
-        if self.config["disable"]:
-            print("Will disable the VHOST.")
+
+        if os_is_supported() and server_is_supported():
+            if self.config["disable"]:
+                print("Will disable the VHOST.")
+            else:
+                print("Setting up...")
         else:
-            print("Setting up...")
+            sys.exit()
 
     def enable():
+        pass
+
+    def write_apache2_conf():
         pass
---
+++ vhost/system_support.py
@@ -0,0 +1,14 @@
+import os
+from sys import platform
+
+def os_is_supported():
+    if platform != 'linux' or platform != 'linux2':
+        print("Only the Linux OS is supported.")
+        return False
+    return True
+
+def server_is_supported():
+    if not os.path.isdir("/etc/apache2"):
+        print("Only Apache2 is supported.")
+        return False
+    return True
