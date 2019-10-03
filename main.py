6ff362f464e95e7aac4195eaa71ea78db2f1f13c
Tomek <tompe97@tlen.pl>
2019-10-03 20:30:51 +0100

    Add basic functionality

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-"""
+#!/usr/bin/python
 
-Need:
+import argparse
+import config
+from option_parser import define_options
+from VHostManager import VHostManager
 
-1. Web Root [/var/www/html]
-2. Domain name
-3. TLD [local]
+def main():
+    user_config = config.DEFAULTS
 
-"""
+    available_options = config.AVAILABLE_OPTIONS
 
+    parser = argparse.ArgumentParser(description="Parse options")
+
+    parser = define_options(parser)
+
+    arguments = parser.parse_args()
+
+    user_config = config.update(user_config, arguments)
+
+    manager = VHostManager(user_config)
+
+    manager.init()
+
+if __name__ == "__main__":
+    main()
