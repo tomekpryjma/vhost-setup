a28716f0f97045f74f599f0da166d4cf0e777f4c
Tomek <tompe97@tlen.pl>
2019-10-04 18:15:21 +0100

    Remove references to user_config

--- main.py
+++ main.py
@@ -1,24 +1,16 @@
-#!/usr/bin/python
-
 import argparse
 import config
 from option_parser import define_options
 from vhost.VHostManager import VHostManager
 
 def main():
-    user_config = config.DEFAULTS
-
-    available_options = config.AVAILABLE_OPTIONS
-
     parser = argparse.ArgumentParser(description="Parse options")
 
     parser = define_options(parser)
 
     arguments = parser.parse_args()
 
-    user_config = config.update(user_config, arguments)
-
-    manager = VHostManager(user_config)
+    manager = VHostManager(arguments)
 
     manager.init()
 
