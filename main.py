59d28b32d301e5bb756161b80e4ad80da88de313
Tomek <tompe97@tlen.pl>
2019-10-04 18:43:45 +0100

    Set working_directory var & parse into VHostManager

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
+import os 
 import argparse
 from option_parser import define_options
 from vhost.VHostManager import VHostManager
 
 def main():
+    working_directory = os.path.dirname(os.path.realpath(__file__))
+
     parser = argparse.ArgumentParser(description="Parse options")
 
     parser = define_options(parser)
 
     arguments = parser.parse_args()
 
-    manager = VHostManager(arguments)
+    manager = VHostManager(arguments, working_directory)
 
     manager.init()
 
