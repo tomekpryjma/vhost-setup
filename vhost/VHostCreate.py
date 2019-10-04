9ec0d03f0874bce9d13984b0f94ab4f7c947c5a1
Tomek <tompe97@tlen.pl>
2019-10-04 18:44:57 +0100

    Add functionality to copy sample conf

--- vhost/VHostCreate.py
+++ vhost/VHostCreate.py
@@ -1,3 +1,15 @@
+import shutil
+
 class VHostCreate:
-    def __init__(self):
-        print("VHostCreate init")
+    def __init__(self, working_directory):
+        self.working_directory = working_directory
+        self.sample_conf = self.working_directory + "/sample/vhost.txt"
+        self.temp_conf = self.working_directory + "/sample/tmp.vhost.conf"
+
+        self.create_temporary_conf_from_sample()
+
+    def create_temporary_conf_from_sample(self):
+        shutil.copy(self.sample_conf, self.temp_conf)
+
+    def write_apache2_conf(self):
+        pass
