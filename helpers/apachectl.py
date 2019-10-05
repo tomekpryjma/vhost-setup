2ce91bfb9ab6ca1585670d2e35371fea37dc0684
Tomek <tompe97@tlen.pl>
2019-10-05 01:52:55 +0100

    Refactor

--- helpers/apachectl.py
+++ helpers/apachectl.py
@@ -1,9 +1,9 @@
 import os
 
 def apachectl(vhost_name):
-    configtest_passed = os.system("sudo apachectl configtest")
-    a2ensite_passed = a2ensite(vhost_name)
-    systemctl_reload_apache2_passed = systemctl_reload_apache2()
+    os.system("sudo apachectl configtest")
+    a2ensite(vhost_name)
+    systemctl_reload_apache2()
 
 def a2ensite(vhost_name):
     return os.system("sudo a2ensite " + vhost_name)
