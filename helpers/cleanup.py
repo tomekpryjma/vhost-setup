4af92d3a3480f0f5dd113bad5d863f18864020ec
Tomek <tompe97@tlen.pl>
2019-10-05 01:53:03 +0100

    Refactor

--- helpers/cleanup.py
+++ helpers/cleanup.py
@@ -1,11 +1,19 @@
 import os
 import config
 from helpers.copy_file import copy_file
+import helpers.apachectl as apachectl
 
 def cleanup(arguments, working_directory):
-    remove_samples(working_directory)
-    remove_apache2_vhost(arguments)
-    remove_hosts_entries(arguments)
+    if os.path.isfile(config.APACHE2_SITES_AVAILABLE + arguments.domain_name + ".conf"):
+        apachectl.a2dissite(arguments.domain_name + ".conf")
+        remove_samples(working_directory)
+        remove_apache2_vhost(arguments)
+        remove_hosts_entries(arguments)
+        apachectl.systemctl_reload_apache2()
+    else:
+        print("Nothing of this site is left to clean")
+
+    print("Cleanup complete!")
 
 def remove_samples(working_directory):
     sample_directory = working_directory + "/sample/"
@@ -14,11 +22,15 @@ def remove_samples(working_directory):
         if file.endswith(".conf"):
             os.remove(sample_directory + file)
 
+    print("Sample files removed")
+
 def remove_apache2_vhost(arguments):
     vhost_file = config.APACHE2_SITES_AVAILABLE + arguments.domain_name + ".conf"
 
     if os.path.exists(vhost_file):
         os.remove(vhost_file)
+
+    print("Apache2 conf file removed")
 
 def remove_hosts_entries(arguments):
     vhost_filename = arguments.domain_name + "." + arguments.tld
@@ -37,6 +49,9 @@ def remove_hosts_entries(arguments):
                 if line.strip("\n") not in [hosts_entry, hosts_entry_alias]:
                     hosts.write(line)
         os.remove(hosts_backup)
+
+
+        print("Hosts file entries removed")
 
     else:
         raise Exception("Failed to create a backup hosts file. Aborting.");
